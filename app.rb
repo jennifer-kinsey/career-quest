c6483bae148d5a1de172e33c654b387b2b8c795c
Asia Kane and Spencer Alan Ruiz <spencer@>
2017-05-15 13:51:24 -0700

    add routing to app.rb to post to sessions upon user login

--- app.rb
+++ app.rb
@@ -2,12 +2,19 @@ require "bundler/setup"
 Bundler.require :default
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+use Rack::Session::Cookie, :secret => '877de719-65d4-4f40-83bf-3b83d56d40db'
+
 get "/" do
   erb :index
 end
 
 get "/sessions/login" do
   erb :"/sessions/login"
+end
+
+get "/users/home" do
+  @user = UserCredential.find(session[:user])
+  erb :"/users/home"
 end
 
 get "/registrations/signup" do
@@ -17,9 +24,19 @@ end
 post "/registrations" do
   @user = UserCredential.create({email: params["user-email"], password: params["user-password"]})
   if @user.errors.any?
-    erb :"/registrations/error"
+    erb :"/error"
   else
-    sessions[:user] = @user.id
+    session[:user] = @user.id
+    redirect "/users/home"
+  end
+end
+
+post "/sessions" do
+  @user = UserCredential.find_by(email: params["user-email"], password: params["user-password"])
+  if @user.errors.any?
+    erb :"/error"
+  else
+    session[:user] = @user.id
     redirect "/users/home"
   end
 end
